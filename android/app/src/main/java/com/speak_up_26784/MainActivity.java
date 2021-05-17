b2735f564ee30fa27b45bfc7a42328422b40ae0f
Crowdbotics <team@crowdbotics.com>
2021-05-17 08:56:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/speak_up_26784/MainActivity.java
@@ -0,0 +1,15 @@
+package com.speak_up_26784;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "speak_up_26784";
+  }
+}
